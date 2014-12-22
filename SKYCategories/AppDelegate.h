6e4c6161e6fa87f5d244b17d9d7b6b8264515ac4
Sky <liumbsky@163.com>
2014-12-22 20:06:00 +0800

    一些常用的类目

---
+++ SKYCategories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKYCategories
+//
+//  Created by SKY on 14/12/22.
+//  Copyright (c) 2014年 SKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKYCategories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKYCategories
+//
+//  Created by SKY on 14/12/22.
+//  Copyright (c) 2014年 SKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
